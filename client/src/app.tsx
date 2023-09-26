f1b0694c49df9dbf5aeb3cc052152faf9624a1a3
Russell McWhae <russell.mcwhae@gmail.com>
2023-09-25 19:34:06 -0700

    Style updates

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,5 +3,9 @@ import { useQuotations } from './hooks/use-quotations'
 export function App() {
 	const { quotations, isLoading, mutate } = useQuotations()
 	console.log('quotations', quotations)
-	return <>Quotation Manager</>
+	return (
+		<>
+			<h3>Quotation Manager</h3>
+		</>
+	)
 }
