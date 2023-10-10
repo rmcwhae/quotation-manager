02ddab78d61615929316abf52977f36c8e6cb8d5
Russell McWhae <russell.mcwhae@gmail.com>
2023-10-09 22:04:51 -0600

    Style updates

--- client/src/components/Quotation.tsx
+++ client/src/components/Quotation.tsx
@@ -7,21 +7,16 @@ type Props = {
 export const Quotation = ({ quotation }: Props) => {
 	const { content, start_page, end_page } = quotation
 	let pageText = ''
+
 	if (start_page && end_page) {
 		pageText = `(${start_page}–${end_page})`
 	} else if (start_page) {
 		pageText = `(${start_page})`
 	}
+
 	return (
-		<div style={styles.content}>
+		<div className="quotation">
 			{content} {pageText}
 		</div>
 	)
-}
-
-const styles = {
-	content: {
-		marginLeft: 'var(--s1)',
-		marginBottom: 'var(--s0)',
-	},
 }
